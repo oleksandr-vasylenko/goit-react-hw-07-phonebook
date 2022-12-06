449a91b7e2aa437c2c0c8be66aa5cfb62f7585fe
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-12-06 12:34:30 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,19 @@
-export const App = () => {
-  return <div></div>;
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  render() {
+    return (
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+    );
+  }
+}
