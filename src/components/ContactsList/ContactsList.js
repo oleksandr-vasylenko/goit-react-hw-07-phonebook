fa5e885ca5cfa902c1785d350bfb21eccf181879
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-12-20 01:14:29 +0200

    Update ContactsList.js

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -2,16 +2,15 @@ import PropTypes from 'prop-types';
 import { Contact } from '../Contacts/Contact';
 import { ContactsThumb } from './ContactsList.Styled';
 
-export const ContactsList = ({ items, onDelete, id }) => {
+export const ContactsList = ({ items, onDelete }) => {
   return (
     <ContactsThumb>
       {items.map(item => (
         <Contact
           name={item.name}
           number={item.number}
-          id={item.id}
-          onDelete={onDelete}
           key={item.id}
+          onDelete={onDelete}
         />
       ))}
     </ContactsThumb>
