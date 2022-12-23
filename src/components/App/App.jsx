a03c9cbe6c6adba0eea0f549b1e8e1678304d38e
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-12-23 15:24:40 +0200

    Adds adding to the local storage methods

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -17,6 +17,21 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = (name, number) => {
     this.setState(prevState => ({
       contacts: [...prevState.contacts, { id: nanoid(), name, number }],
